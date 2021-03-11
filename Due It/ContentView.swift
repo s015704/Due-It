99d8c5c51d4d5fd703f80c0cddc586e9fbbc56b8
Annika Naveen <s015704@students.lmsd.org>
2021-03-11 12:11:41 -0500

    added .gitignore

---
+++ Due It/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Due It
+//
+//  Created by Annika Naveen (student LM) on 3/11/21.
+//  Copyright © 2021 Annika Naveen (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Due ItTests/Due_ItTests.swift
@@ -0,0 +1,34 @@
+//
+//  Due_ItTests.swift
+//  Due ItTests
+//
+//  Created by Annika Naveen (student LM) on 3/11/21.
+//  Copyright © 2021 Annika Naveen (student LM). All rights reserved.
+//
+
+import XCTest
+@testable import Due_It
+
+class Due_ItTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
